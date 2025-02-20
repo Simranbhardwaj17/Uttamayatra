5daaf3a2f547db53462bb9c890dbc468b193b28e
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-02-20 15:11:36 +0530

    Install gsap,  used useGSAP hook for panel open/close

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import { useGSAP } from '@gsap/react';
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel';
 
 const Home = () => {
   const [pickup, setPickup] = useState('')
   const [destination, setDestination] = useState('')
+  const [panelOpen, setPanelOpen] = useState(false)
+  const panel = useRef(null)  //for naming the div
+  const panelCloseRef = useRef(null)
 
   const submitHandler = () => {
     e.preventDefault()
   }
+
+  useGSAP(function () {
+    if(panelOpen) {
+      gsap.to(panelRef.current, {
+        height:'70%',
+        padding: 24
+      })
+      gsap.to(panelCloseRef.current, {
+        opacity:1
+      })
+    } else {
+      gsap.to(panelRef.current, {
+        height:'0%',
+        padding: 0
+      })
+      gsap.to(panelCloseRef.current, {
+        opacity:0
+      })
+    }
+  }, [panelOpen])
+
+
   return (
     <div className='h-screen relative'>
       <img className='w-16 absolute left-5 top-5' src="https://i.pinimg.com/736x/a1/55/fa/a155fa4e9ae945f9faf0bc2430b2a140.jpg" alt= "" />
@@ -14,13 +42,23 @@ const Home = () => {
       <img className='h-full w-full object-cover' src="https://thegadgetflow.com/wp-content/uploads/2020/03/Google-Maps-vs-Google-Earth-featured.jpg" alt= "" />
       </div>
       <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
-        <div className='h-[30%] p-5 bg-white relative'>
+        <div className='h-[30%] p-6 bg-white relative'>
+          <h5 ref={ panelCloseRef }    //ref used to select ele
+          onClick={() => {
+            setPanelOpen(false)
+          }}  
+          className='absolute opacity-0 top-6 right-6 text-2xl'>
+            <i className='ri-arrow-down-wide-line'></i>
+          </h5>
           <h4 className='text-2xl font-semibold'>Find a trip</h4>
           <form onSubmit={(e)=>{
             submitHandler(e)
           }}>
             <div className='line absolute h-16 w-1 top-[45%] left-10 bg-gray-700 rounded-full'></div>
             <input 
+            onClick= {()=> {
+              setPanelOpen(true)
+            }}
             value = {pickup}
             onChange={(e) => {
               setPickup(e.target.value)
@@ -29,6 +67,9 @@ const Home = () => {
             type ="text" 
             placeholder='Add a pick-up location' /> 
             <input 
+            onClick= {()=> {
+              setPanelOpen(true)
+            }}
             value = {destination}
             onChange={(e) => {
               setDestination(e.target.value)
@@ -38,7 +79,9 @@ const Home = () => {
             placeholder='Enter your destination' /> 
           </form>
         </div>
-        <div className='bg-red-500'></div>
+        <div ref={panelRef} className='bg-white h-0'>
+          <LocationSearchPanel />
+        </div>
       </div>
     </div>
   )
