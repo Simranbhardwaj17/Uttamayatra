61aba353d632da57815d83cfb30ea1d859f8ce92
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-02-25 09:39:53 +0530

    User riding & riding Route created

---
+++ Frontend/src/pages/Riding.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Riding = () => {
+  return (
+    <div className='h-screen'>
+      <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+        <i className="text-3xl font-medium ri-home-heart-fill"></i>
+      </Link>
+      <div className='h-1/2'>
+        <img className='h-full w-full object-cover' src="https://thegadgetflow.com/wp-content/uploads/2020/03/Google-Maps-vs-Google-Earth-featured.jpg" alt= "" />
+      </div>
+
+      <div className='h-1/2 p-4'>
+        <div className='flex items-center justify-between'>
+          <img className='h-12' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpgslkhHmZ9MvM1dVBrM9RDg-Ov-zcg23Vjg&s" alt="Car logo" />
+          <div className='text-right'>
+            <h2 className='text-lg font-medium'>Shailesh</h2>
+            <h4 className='text-xl font-semibold -mt-1 -mb-1'>GJ02 AB 1234</h4>
+            <p className='text-sm text-gray-600'>Mercedes Benz</p>
+          </div>
+        </div>
+
+        <div className='flex gap-2 justify-between flex-col items-center'>
+          <div className='w-full mt-5'>
+
+            <div className='flex items-center gap-5 p-3 border-b-1'>
+              <i class="ri-map-pin-user-fill"></i>
+              <div>
+                <h3 className='text-lg font-medium'>5A, Sharang's cafe</h3>
+                <p className='text-sm -mt-1 text-gray-600'>Sangam, Ayodhya</p>
+              </div>
+            </div>
+
+            <div className='flex items-center gap-5 p-3 '>
+              <i className="ri-money-rupee-circle-fill"></i>
+              <div>
+                <h3 className='text-lg font-medium'>₹180.93</h3>
+                <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button className='w-full bg-green-600 text-white font-semibold p-2 rounded-lg'>Make a payment</button>
+      </div>
+    </div>
+  )
+}
+
+export default Riding
