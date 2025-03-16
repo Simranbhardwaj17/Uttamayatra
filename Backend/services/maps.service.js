9f994715da9bdef0e8d66fa080126066ed1fbd0f
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-03-16 22:10:15 +0530

    Location radius

--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const captainModel = require('../models/captain.model')
 
 module.exports.getAddressCoordinate = async (address) => {
   const apiKey = process.env.GOOGLE_MAPS_API;
@@ -71,4 +72,19 @@ module.exports.getAutoCompleteSuggestions = async (input) => {
     console.error(err);
     throw err;
   }
+}
+
+
+module.exports.getCaptainsInTheRadius = async (ltd, lng, radius) => {
+  const captains = await captainModel.find({
+    location: {
+      //these r defined by mongoDB & we use
+      $geoWithin: {  
+        $centerSphere: [ [ ltd, lng ], radius / 3963.2 ]
+      }
+    }
+  });
+
+  return captains;
+
 }
