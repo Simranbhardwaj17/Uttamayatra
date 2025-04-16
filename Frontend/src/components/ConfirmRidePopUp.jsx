7d0967b55feddb97951eaa1786df828dd6fb1b43
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-04-16 21:54:35 +0530

    ConfirmRidePopUp panel on FE

--- Frontend/src/components/ConfirmRidePopUp.jsx
+++ Frontend/src/components/ConfirmRidePopUp.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const ConfirmRidePopUp = (props) => {
   const [otp, setOtp] = useState('')
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault()
+
+    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/rides/start-ride`, {
+      params: {
+        rideId: props.ride._id,
+        otp: otp
+      },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+
+    if (response.status === 200) {
+      props.setConfirmRidePopupPanel(false)
+      props.setRidePopupPanel(false)
+      navigate('/captain-riding', { state: { ride: props.ride } })
+    }
   }
 
   return (
@@ -53,9 +71,7 @@ const ConfirmRidePopUp = (props) => {
         </div>
         
         <div className='mt-6 w-full'>
-          <form onSubmit={(e) => {
-            submitHandler(e)
-          }}>
+          <form onSubmit={submitHandler}>
             <input 
               value = {otp}
               onChange={(e) => setOtp(e.target.value)}
